feeb020e4d507cc02362d4ee360bb77fb994e8b9
caviardr <caviardr@gmail.com>
2026-09-06 10:28:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
-window.JAVANO_MARKET_CONFIG={
-  SUPABASE_URL:"https://ijmkllpnlngrftbaqzed.supabase.co",SUPABASE_PUBLISHABLE_KEY:"sb_publishable_Xu9S6l4c66mY0UybERun1w_h02O-eQr,APP_URL:"https://caviardr.github.io/javano-marketplace/",CS_WHATSAPP:"6281511060808",POINT_TO_RUPIAH:1,MIN_WITHDRAW_RUPIAH:100000
+window.JAVANO_MARKET_CONFIG = {
+  SUPABASE_URL: "https://ijmkllpnlngrftbaqzed.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_Xu9S6l4c66mY0UybERun1w_h02O-eQr",
+  APP_URL: "https://caviardr.github.io/javano-marketplace/",
+  CS_WHATSAPP: "6281511060808",
+  POINT_TO_RUPIAH: 1,
+  MIN_WITHDRAW_RUPIAH: 100000
 };
