09228127feb3545167dd459a8cdbebfe05e1ce37
caviardr <caviardr@gmail.com>
2026-09-06 11:44:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.JAVANO_MARKET_CONFIG = {
   SUPABASE_URL: "https://ijmkllpnlngrftbaqzed.supabase.co",
   SUPABASE_PUBLISHABLE_KEY: "sb_publishable_Xu9S6l4c66mY0UybERun1w_h02O-eQr",
-  APP_URL: "https://caviardr.github.io/javano-marketplace/",
+  APP_URL: "https://caviardr.github.io/Javano-ID-Marketplace/",
   CS_WHATSAPP: "6281511060808",
   POINT_TO_RUPIAH: 1,
   MIN_WITHDRAW_RUPIAH: 100000
